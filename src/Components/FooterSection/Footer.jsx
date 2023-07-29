efa861182e2bb22f3d904c9a3793b25923b1fa56
Gloria BADOHOUN <140901966+devbaged@users.noreply.github.com>
2023-07-29 18:32:10 +0000

    footer update

--- src/Components/FooterSection/Footer.jsx
+++ src/Components/FooterSection/Footer.jsx
@@ -7,9 +7,12 @@ const Footer = () => {
     grid grid-clos-5 m-auto items-center justify-center' >
       <div>
       <div className="logoDiv">
-        <h1 className="logo text-[25px] text-primary">
+        <h1 className="logo text-[25px] text-white pb-[1.5rem] ">
           <strong>Stage</strong>Link
         </h1>
+        <p className='text-white pb-[13px] opacity-70 leading-7' >
+          salut je suis la description du footer
+        </p>
       </div>
       </div>
     </div>
