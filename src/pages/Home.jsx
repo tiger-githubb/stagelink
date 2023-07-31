f0115854bf77ddd63f8e8d43eebf18b808c9a1d7
KARBOU Aristide <98205891+tiger-githubb@users.noreply.github.com>
2023-07-31 19:28:26 +0000

    cleaning

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,12 +11,7 @@ import Stage from '../Components/StageSection/Stage';
 const Home = () => {
   const navigate = useNavigate();
   const auth = getAuth();
-  const handlelogout = async() => {
-    await signOut(auth);
-    localStorage.removeItem('token');
-    localStorage.removeItem('user');
-    navigate("/login");
-  }
+  
   return (
     <div>
       < NavBar />
@@ -24,7 +19,6 @@ const Home = () => {
       < Stage />
       < Value />
       < Footer />
-      <button onClick={handlelogout} className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">deconnexion</button>
     </div>
   )
 }
